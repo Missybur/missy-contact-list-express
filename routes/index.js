269d2349f61d133288e14d6b910c4612f73a80aa
Missy Bur <Missybur@gmail.com>
2015-11-12 09:16:05 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,16 @@
 
 var express = require('express');
 var router = express.Router();
+var Contact = require('../models/contact');
+
 
 router.get('/', function(req, res){
-  res.render('index');
+  console.log("in index.js")
+  Contact.find(function(err, contacts) {
+    console.log(contacts)
+    if(err) return res.status(400).send(err);
+    res.render("index", {title: "Contact List", items: contacts});
+  });
 });
-
-
 
 module.exports = router;
